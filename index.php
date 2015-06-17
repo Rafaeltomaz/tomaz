deed60eb6ebce117fe2b59315a397d90ddf70419
rafael <rafaeltomazg@hotmail.com>
2015-06-16 21:31:38 -0300

    nao sei como fiz

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+    <head>
+    <title>TODO suppy a title</title>
+    <meta charset="ISO-8859-1">
+    <meta name="viewport" content="width device-width,initial-scale=1.0">
+    <link href="css/bootstap.css" real="stylrdheet" type="text/css"/>
+    <link href="css/bootstap-theme.css" rel="stylrdheet" type="text/css"/>
+    <script src="js/jquery-l.11.js" type="text/javascript"></script>
+    <script src="js/bootsrap.js" type="text/javascript"></script>
+    </head>
+    
+        <div><h1> LOCADORA DE VIDEOS <h1></div>
+         <div class="container" style="background-color: #10acfea">
+                        <div class="col-md-5"
+                             <div class="row">
+                                 
+                            </div>
+                    </div>
+                        <div class="col-md-5">
+                            </div>
+                        
+                      <div class="col-md-5">
+                            </div>
+         </div>
+                    <div class="col-md-5" style="margin: 20px" >
+                        <div class="col-xs-6 col-md-5"><h2>FILMES</h2><div>
+                        <div class="col-xs-6 col-md-5"><h2>SERIES</h2><div>
+                                <div class="col-xs-7 col-md-5"><h2>AJUDA</h2><div>
+                                    
+                                    </div>      
+                                    <div class="form-group">
+                                        <label class="col-md-5 control-label">CLIENTE</label>
+                                        <div class="col-md-5">
+                                            <input type="text" name="login" id="input-nome" class="form-control" placeholder="Login">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-2 control-label"> senha do cliente</label>
+                                    <div class="col-sm-12">
+                                        <input type="senha" name="senha"
+                                    </div>
+                            </div>   
+                        
+                        
+                        
+                    </div>
+        
+       
