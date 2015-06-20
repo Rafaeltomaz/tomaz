f51455f4ead88c3d914118ad25abca7133d155a0
rafael <rafaeltomazg@hotmail.com>
2015-06-19 21:43:04 -0300

    wtf

--- index.php
+++ index.php
@@ -1,47 +1,147 @@
 <html>
+
     <head>
-    <title>TODO suppy a title</title>
-    <meta charset="ISO-8859-1">
-    <meta name="viewport" content="width device-width,initial-scale=1.0">
-    <link href="css/bootstap.css" real="stylrdheet" type="text/css"/>
-    <link href="css/bootstap-theme.css" rel="stylrdheet" type="text/css"/>
-    <script src="js/jquery-l.11.js" type="text/javascript"></script>
-    <script src="js/bootsrap.js" type="text/javascript"></script>
+        <title>thiherber</title>
     </head>
-    
-        <div><h1> LOCADORA DE VIDEOS <h1></div>
-         <div class="container" style="background-color: #10acfea">
-                        <div class="col-md-5"
-                             <div class="row">
-                                 
-                            </div>
-                    </div>
-                        <div class="col-md-5">
-                            </div>
-                        
-                      <div class="col-md-5">
-                            </div>
-         </div>
-                    <div class="col-md-5" style="margin: 20px" >
-                        <div class="col-xs-6 col-md-5"><h2>FILMES</h2><div>
-                        <div class="col-xs-6 col-md-5"><h2>SERIES</h2><div>
-                                <div class="col-xs-7 col-md-5"><h2>AJUDA</h2><div>
-                                    
-                                    </div>      
-                                    <div class="form-group">
-                                        <label class="col-md-5 control-label">CLIENTE</label>
-                                        <div class="col-md-5">
-                                            <input type="text" name="login" id="input-nome" class="form-control" placeholder="Login">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-2 control-label"> senha do cliente</label>
-                                    <div class="col-sm-12">
-                                        <input type="senha" name="senha"
-                                    </div>
-                            </div>   
-                        
-                        
-                        
-                    </div>
+    <body background="img/imagem-imagem.jpg" alt=""/>
         
-       
+        <table border="1" width="100%">
+            <tr>
+                <td width="1000">
+                    <h1 align="center">
+                        BEM VINDO AO MENOR SITE DE COMPRAS DE CERVEJAS               
+                        <img  src= "img/images.jpg"  alt= "img/"  class= "img-rounded" >
+                        <div class="container-fluid">
+
+
+                            <marquee behavior="alternate">
+                                <left>
+                                    <font face="Comic Sans MS" size="5" color="#ff0000">
+                                    <b>
+                                        <blink>
+                                            <img src="img/abcde.jpg" alt=""/>
+                                            APENAS UM SITE DE CERVEJA XXXXXXXX
+                                            <img src="img/abcde.jpg" alt=""/>
+
+                                        </blink>
+                                    </b>
+                                    </font>
+                                </left>
+                            </marquee>
+
+                        </div>       
+                    </h1>
+                </td>
+            </tr>
+        </table>
+
+        <table border="1" width="100%">
+            <tr>
+                <td height="500" width="137" valign="top"><br>
+
+                    <a href="">COMPRAS 1</a> <br><br>
+                    <a href="">COMPRAS 2</a> <br><br>
+                    <a href="">COMPRAS 3</a> <br><br>
+                    <a href="">COMPRAS 4</a> <br><br>
+                    <a href="">COMPRAS 5</a> <br><br>
+                    <a href="">COMPRAS 6</a> <br><br><br><br>
+
+
+                    <font face="Comic Sans MS" size="5" color="#000000">
+                    <b>
+                        <blink>
+
+                            ITENS ADQUIRIDOS 
+
+                        </blink>
+                    </b>
+                    </font>
+
+
+                    <img src="img/0000101101101.jpg" alt=""/>
+                    <hr></td>
+                <td>
+                </td>
+                <td width="700">
+                    Conteudo
+                    <table border="1" width="100%"></table>
+                </td>
+                <td>
+
+                </td>
+                <td height="500" width="137">
+
+
+            <left>
+                <font face="Comic Sans MS" size="5" color="#0000FF">
+                <b>
+                    <blink>
+
+                        O Site foi Patrocinado pelas cervejas 
+
+                    </blink>
+                </b>
+                </font>
+            </left>
+            <a href="http://www.skol.com.br/" style="color: #000"><h2>SKOL</h2></a><span></span>
+            <a href="http://bavaria.com.br/institucional/?v=0c69e41e" style="color: #000"><h2>BAVARIA</h2></a><span></span>
+            <a href="http://www.heinekenbrasil.com.br/idade.aspx" style="color: #000"><h2>HEINEK</h2></a><span></span>
+            <a href="http://www.cervejacrystal.com.br/" style="color: #000"><h2>CRYSTAL</h2></a><span></span>
+            <a href="http://www.cervejaitaipava.com.br/" style="color: #000"><h2>ITAIPAVA</h2></a><span></span>
+
+
+        </td>
+    </tr>
+</table>
+
+<table border="1" width="100%">
+    <tr>
+        <td width="1000">
+            <form action="http://webmail.cardiomedh.com.br/src/loginremoto.php" method="post"> 
+
+                <input type="hidden" name="dominio" value="cardiomedh.com.br"> 
+                <label for="nome">Usu&aacute;rio</label> 
+                <input type="text" name="login_username" id="login_username" class="text" /> <span class="dominio">@Hotmail.com.br</span><br /> 
+                <div>&nbsp</div> 
+                <label for="senha">Senha</label> 
+                <input type="password" name="secretkey" id="secretkey" class="text" onblur="document.getElementById('tooltip').style.display = 'none';" onfocus="document.getElementById('tooltip').style.display = 'block';" /> 
+                <input type="submit" value="Entrar" class="submit" /> 
+
+            </form>
+    </tr>
+</table>
+<table border="1" width="100%">
+    <tr>
+        <td width="1000">
+            RESPONSAVEL: RAFAEL TOMAZ  EMAIL: RAFAELTOMAZG@HOTMAIL.COM 
+        </td>
+    </tr>
+</table>
+
+<div class="container-fluid">
+
+
+    <marquee behavior="alternate">
+        <left>
+            <font face="Comic Sans MS" size="5" color="#000000">
+            <b>
+                <blink>
+
+                    UM TEXTO LEGAL 
+
+                </blink>
+            </b>
+            </font>
+        </left>
+    </marquee>
+
+</div>
+
+UM TEXTO LEGAL   UM TEXTO LEGAL   UM TEXTO LEGAL   UM TEXTO LEGAL   UM TEXTO LEGAL   UM TEXTO LEGAL   UM TEXTO LEGAL 
+
+
+</body>
+
+
+
+</html>
